d620f0ee2992d491dca9694ab032b901524d23d0
theshaquib <114143938+theshaquib@users.noreply.github.com>
2023-04-25 16:04:15 +0530

    testing

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./button.css";
+
+const button = () => {
+  return (
+    <a className='home' href="https://shaquibsiddique.in/">{'<<'}</a>
+  )
+}
+
+export default button
---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import SubHeading from './SubHeading/SubHeading';
+import Newsletter from './Footer/Newsletter';
+import FooterOverlay from './Footer/FooterOverlay';
+import MenuItem from './Menuitem/MenuItem';
+import Navbar from './Navbar/Navbar';
+import Button from "./Button/Button" 
+
+export {
+  SubHeading,
+  Newsletter,
+  FooterOverlay,
+  MenuItem,
+  Navbar,
+  Button
+};
